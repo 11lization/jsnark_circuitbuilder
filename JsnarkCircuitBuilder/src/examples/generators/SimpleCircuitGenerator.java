fe857ac0127c39fb150885778308898707a195e9
11lization <11lization@naver.com>
2022-07-04 01:30:49 +0000

    SHA256

--- JsnarkCircuitBuilder/src/examples/generators/SimpleCircuitGenerator.java
+++ JsnarkCircuitBuilder/src/examples/generators/SimpleCircuitGenerator.java
@@ -19,25 +19,28 @@ public class SimpleCircuitGenerator extends CircuitGenerator {
 	protected void buildCircuit() {
 
 		// declare input array of length 4.
-		inputs = createInputWireArray(4);
+		inputs = createInputWireArray(3);
 
 		// r1 = in0 * in1
 		Wire r1 = inputs[0].mul(inputs[1]);
 
 		// r2 = in2 + in3
-		Wire r2 = inputs[2].add(inputs[3]);
+		// Wire r2 = inputs[2].add(inputs[3]);
 
 		// result = (r1+5)*(6*r2)
-		Wire result = r1.add(5).mul(r2.mul(6));
+		Wire result = r1.isEqualTo(inputs[2]);
+
+		// result = input[0]*input[1]+input[2]
 
 		// mark the wire as output
+		
 		makeOutput(result);
 
 	}
 
 	@Override
 	public void generateSampleInput(CircuitEvaluator circuitEvaluator) {
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 3; i++) {
 			circuitEvaluator.setWireValue(inputs[i], i + 1);
 		}
 	}
